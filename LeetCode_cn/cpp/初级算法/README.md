ebf33e781c1c7e0a620424314c4cbd57afe80060
magic-wei <wei_company@126.com>
2018-06-29 23:33:16 +0800

    merge Repos 'practice_cpp', 'practice_python' and 'practice_markdown' into one Repo #2.

--- LeetCode_cn/cpp/初级算法/README.md
+++
@@ -1,3 +0,0 @@
-# 初级算法 章节
-* [章节网址](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/)
-
--- example_project/src/example1.cpp
+++
@@ -1,7 +0,0 @@
-#include "lib1.hpp"
-int main( int argc, char **argv )
-{
-    example_project::Lib1 object;
-    object.sayHelloFromLib1();
-    return 0;
-}
