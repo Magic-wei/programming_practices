eb6c67e60296146879ca399f141b72f769413688
magic-wei <wei_company@126.com>
2018-08-04 18:56:55 +0800

    update temp_test.

--- practice_cpp/func/src/temp_test.cpp
+++ practice_cpp/func/src/temp_test.cpp
@@ -3,19 +3,10 @@
 //
 
 #include <iostream>
-#include <list>
 
 using namespace std;
 
-void Kalman_filter_test(void){
-    // TODO: add kalman filter test here, if possible, move to a individual main function.
-    // Here I finish a structure, I will use this function when I want to test kalman filter. Next time I will keep on it.
-}
-
 int main(){
-
-    // OK, maybe I will do this next time, so I just build the structure function here.
-    Kalman_filter_test();
 
     return 0;
 }
