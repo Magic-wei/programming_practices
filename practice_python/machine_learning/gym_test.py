b134b67a465aace9189b7731489cefe33f52a368
magic-wei <wei_company@126.com>
2019-03-25 09:29:01 +0800

    modify gym example test

--- practice_python/machine_learning/gym_test.py
+++ practice_python/machine_learning/gym_test.py
@@ -18,7 +18,8 @@ for i_episode in range(20):
 
 import gym
 env = gym.make('CartPole-v0')
-for i_episode in range(20):
+iter_num = 50;
+for i_episode in range(iter_num):
     observation = env.reset()
     for t in range(100):
         env.render()
@@ -27,6 +28,6 @@ for i_episode in range(20):
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after {} timesteps".format(t+1))
-            print((i_episode+1), "/", 20)
+            print((i_episode+1), "/", iter_num)
             break
 env.close()
